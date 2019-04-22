3685bf813439dd7b999b8e28a137fb85e1c8d8ea
junjie.xun@oriente.com-at-120896031351 <junjie.xun@oriente.com-at-120896031351>
2019-04-22 13:42:47 +0800

    test utils read_config

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,2 +1,7 @@
+from reinforch.environments import OpenAIGym
+from reinforch.environments import GymEnv
+from reinforch.environments import Environment
+
+
 def test_import():
     pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,4 @@
-def test_import():
-    pass
-
-
 def test_read_config():
     from reinforch.utils import read_config
-    # s = '{"name":"xjj","age":18}'
-    # assert isinstance(read_config(s), dict)
-    # s = {'name': 'xjj', 'age': 18}
-    # assert isinstance(read_config(s), dict)
-    # # FIXME if run at other Compute
-    # s = '/Users/junjiexun/PycharmProjects/reinforch/tests/config.json'
-    # assert isinstance(read_config(s), dict)
-    pass
-
-def test_from_config():
-    pass
+    s = read_config('tests/configs/test_network_config.json')
+    assert s is not None
